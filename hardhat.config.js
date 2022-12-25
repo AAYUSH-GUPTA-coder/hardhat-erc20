0b26ca381e50ad9f92c5474e95b03e02bb5ed0e0
Aayush Gupta <guptaaayush432@gmail.com>
2022-12-25 18:29:22 +0530

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,10 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
-require("hardhat-deploy");
-require("solidity-coverage");
-require("hardhat-gas-reporter");
-require("hardhat-contract-sizer");
 require("dotenv").config();
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
 const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -29,13 +24,6 @@ module.exports = {
     apiKey: {
       goerli: ETHERSCAN_API_KEY,
     },
-  },
-  gasReporter: {
-    enabled: true,
-    currency: "USD",
-    outputFile: "gas-report.txt",
-    noColors: true,
-    coinmarketcap: COINMARKETCAP_API_KEY,
   },
   solidity: {
     compilers: [
