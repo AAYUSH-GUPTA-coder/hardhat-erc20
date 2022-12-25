e8504fa4a7548d4c67e2e8d94943a0c5d58dd43d
Aayush Gupta <guptaaayush432@gmail.com>
2022-12-25 18:28:01 +0530

    Update

--- contracts/ManualToken.sol
+++ contracts/ManualToken.sol
@@ -17,26 +17,24 @@ contract ManualToken {
   uint8 public decimals = 18;
   // 18 decimals is the strongly suggested default, avoid changing it
   uint256 public totalSupply;
-
-    uint initialSupply;
     mapping (address => uint) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allownace;
+    mapping(address => mapping(address => uint256)) public allowance;
 
-    constructor(uint256 initialSupply, string memory tokenName, string memory tokenSymbol) {
-        totalSupply = initialSupply * 10**uint(decimals);
+    constructor(uint256 _initialSupply, string memory tokenName, string memory tokenSymbol) {
+        totalSupply = _initialSupply * 10**uint(decimals);
         balanceOf[msg.sender] = totalSupply;
         name = tokenName;
-        symbol = tokenSymbol
+        symbol = tokenSymbol;
     }
 
-    function transfer(address from, address to, uint256 amount){
+    function transfer(address from, address to, uint256 amount) public {
         balanceOf[from] -= amount;
         balanceOf[to] += amount;
     }
 
-    function transferFrom(address _from, address _to, uint256 _allowance) public returns(bool success) {
-        require(_value <= allownace[_from][msg.sender]);
-        allownace[_from][msg.sender] -= value;
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success) {
+        require(_value <= allowance[_from][msg.sender]);
+        allowance[_from][msg.sender] -= _value;
         transfer(_from, _to, _value);
         return true;
     }
@@ -46,7 +44,6 @@ contract ManualToken {
     returns (bool success)
   {
     allowance[msg.sender][_spender] = _value;
-    emit Approval(msg.sender, _spender, _value);
     return true;
   }
 }
