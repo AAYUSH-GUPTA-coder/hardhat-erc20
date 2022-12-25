c6eca93c26403b0d059818b614447ebfd04bec79
Aayush Gupta <guptaaayush432@gmail.com>
2022-12-25 18:28:29 +0530

    Create openzeppelin contract

--- contracts/OurToken.sol
+++ contracts/OurToken.sol
@@ -1,10 +1,10 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract OurToken is ERC20 {
     constructor(uint initialSupply) ERC20("OurToken", "OT"){
-        _mint(msg.sender , initialSupply);
+        _mint(msg.sender , initialSupply *10 ** 18);
     }
 }
